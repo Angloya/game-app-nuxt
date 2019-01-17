cc1041b165a6c7563b9d0c2f8ea4544ce7778f56
Alena Borisova <alena.borisova@altenar.com>
2019-01-17 20:51:46 +0300

    added creation of the field size

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -15,17 +15,16 @@
         <h1>{{message}}</h1>
       </div>
       <v-card max-height="500" max-width="500">
-        <v-container v-bind="{ [`grid-list-${size}`]: true }" fluid>
+        <v-container grid-list-sm fluid>
           <v-layout row wrap>
             <v-flex
               v-for="(card, idx) in cards" :key="idx"
               xs3>
               <v-card flat 
               @keyup.left="moveToTheLeft"
-              light
+              lights
               :color="card.active ? card.colorActiveCard : colorCard"
-              height="100"
-              width="100">
+              height="100">
               <v-layout fill-height align-center justify-center>
               <v-flex class="text-xs-center">
                 <h1>{{card.num}}</h1>
@@ -45,142 +44,74 @@
 export default {
   data () {
     return {
-      size: 'lg',
       isGameStarted: false,
+      size: 16,
       colorCard: 'grey darken-2',
       MoveActive: false,
       message: 'Join the numbers and get to the 2048 tile',
-      StringsLeft: {
-        FirstStringX: [3, 2, 1, 0],
-        SecondStringX: [7, 6, 5, 4],
-        TreeStringX: [11, 10, 9, 8],
-        FourdStringX: [15, 14, 13, 12],
+      strings: {
+        StringsLeft: {},
+        StringsRight: {},
+        StringsDown: {},
+        StringsUp: {}
       },
-      StringsRight: {
-        FirstStringX: [0, 1, 2, 3],
-        SecondStringX: [4, 5, 6, 7],
-        TreeStringX: [8, 9, 10, 11],
-        FourdStringX: [12, 13, 14, 15],
-      },
-      StringsDown: {
-        FirstStringX: [0, 4, 8, 12],
-        SecondStringX: [1, 5, 9, 13],
-        TreeStringX: [2, 6, 10, 14],
-        FourdStringX: [3, 7, 11, 15],
-      },
-      StringsUp: {
-        FirstStringX: [12, 8, 4, 0],
-        SecondStringX: [13, 9, 5, 1],
-        TreeStringX: [14, 10, 6, 2],
-        FourdStringX: [15, 11, 7, 3],
-      },
-      cards: [
-        {
-          id: 0,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 1,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 2,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 3,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 4,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 5,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 6,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 7,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 8,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 9,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 10,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 11,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 12,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 13,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 14,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        },
-        {
-          id: 15,
-          active: false,
-          num: null,
-          colorActiveCard: 'teal'
-        }
-      ]
+      cards: []
     }
   },
   methods: {
     startGame () {
       this.isGameStarted = true
+      this.getCards()
+      this.getStringsX()
+      this.getStringsY()
       this.getNewActiveCard()
       this.getNewActiveCard()
+      debugger
       window.addEventListener('keyup', this.logKey)
-      this.cardsClone = this.cards
+    },
+    getStringsX () {
+      var strings = {}
+      for (let i = 0; i < Math.sqrt(this.size); i++) {
+        let string = []
+        var sizeSqrt = Math.sqrt(this.size)*i
+        var num = Math.sqrt(this.size)*(i+1) 
+        for (let item = sizeSqrt; item < num; item++) {
+          string.push(item)
+        }
+        strings[i] = string
+      }
+      this.strings.StringsRight = strings
+      this.strings.StringsLeft = JSON.parse(JSON.stringify(this.strings.StringsRight))
+      for (var string in this.strings.StringsLeft) {
+        this.strings.StringsLeft[string].reverse()
+      }
+    },
+    getStringsY () {
+      var strings = {}
+      for (let i = 0; i < Math.sqrt(this.size); i++) {
+        let string = []
+        var num = Math.sqrt(this.size) 
+        debugger
+        for (let item = i; item < this.size; item+=num) {
+          string.push(item)
+        }
+        strings[i] = string
+      }
+      this.strings.StringsDown = strings
+      this.strings.StringsUp = JSON.parse(JSON.stringify(this.strings.StringsDown))
+      for (var string in this.strings.StringsUp) {
+        this.strings.StringsUp[string].reverse()
+      }
+    },
+    getCards () {
+    for (let item = 0; item < this.size; item++) {
+        let card = {}
+        card.id = item
+        card.active = false,
+        card.num = null,
+        card.colorActiveCard = 'teal'
+        this.cards.push(card)
+      }
     },
     gameOver (message) {
       this.isGameStarted = false
@@ -205,16 +136,20 @@ export default {
     logKey(e) {
       switch (e.keyCode) {
         case 37:
-          this.moveX(this.StringsLeft)
+          this.moveX(this.strings.StringsLeft)
           break;
         case 39:
-          this.moveX(this.StringsRight)
+          this.moveX(this.strings.StringsRight)
           break;
         case 40:
-          this.moveX(this.StringsDown)
+          this.moveX(this.strings.StringsDown)
           break;
         case 38:
-          this.moveX(this.StringsUp)
+          this.moveX(this.strings.StringsUp)
+      }
+      if (this.MoveActive) {
+        this.getNewActiveCard()
+        this.MoveActive = false
       }
     },
     cellActive (nextEl, el, rate) {
@@ -247,10 +182,6 @@ export default {
             }
           }
         }
-      }
-      if (this.MoveActive) {
-      let newActiveCard = this.getNewActiveCard()
-      this.MoveActive = false
       }
     }
   }
